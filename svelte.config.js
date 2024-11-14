057be992474bb2a03dae0aac20ff6b7af48495ec
Tyler Marques <tyler.marques@plailabs.com>
2024-11-13 20:15:28 -0500

    Fixing builds
    I changed the adapter to auto, but forgot that what was powering the
    docker build was the adapter-node. It is needed to run with node.js as
    well as set the build directory.

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
 import remarkToc from 'remark-toc';
@@ -16,7 +16,9 @@ const config = {
     })
   ],
   kit: {
-    adapter: adapter()
+    adapter: adapter({
+      out: 'build'
+    })
   }
 };
 
